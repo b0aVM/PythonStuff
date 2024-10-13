098da1d27a2a34cf54fd47e69f69b7265d81e754
b0aVM <b0a.vlmo@gmail.com>
2024-10-13 16:39:22 +0200

    bool_to_word

---
+++ CodeWars/kyu8/bool_to_word.py
@@ -0,0 +1,14 @@
+"""
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+"""
+s = (True)
+a = (False)
+
+def bool_to_word(boolean):
+    # TODO
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+
+print(bool_to_word(a))
