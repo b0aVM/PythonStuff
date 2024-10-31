2ba07a853430623a59d5293521851436e776cb1c
b0aVM <b0a.vlmo@gmail.com>
2024-10-31 18:34:58 +0100

    binary search method

--- Grokking_algorithms/Chapter.1/binary_search.py
+++ Grokking_algorithms/Chapter.1/binary_search.py
@@ -1,5 +1,14 @@
 """
+
 binary search method
+
+• Binary search is a lot faster than simple search.
+• O(log n) is faster than O(n), but it gets a lot faster once the list of
+items you’re searching through grows.
+• Algorithm speed isn’t measured in seconds.
+• Algorithm times are measured in terms of growth of an algorithm.
+• Algorithm times are written in Big O notation.
+
 """
 
 def binary_search(list, item):
